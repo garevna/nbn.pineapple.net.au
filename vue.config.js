463960de0db3aa1508871618d76c0372c6b658a9
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2020-07-13 06:37:48 +0300

    github ver

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  runtimeCompiler: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/nbn.pineapple.net.au/'
+    : '/'
 }
