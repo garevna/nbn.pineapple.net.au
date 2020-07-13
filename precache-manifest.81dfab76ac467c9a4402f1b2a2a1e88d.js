77fb533c539df60c1fd29f05f5c67b58d3f2f0cf
Irina Fylyppova (via Travis CI) <irina.h.fylyppova@gmail.com>
2020-07-13 03:41:47 +0000

    Deploy garevna/nbn.pineapple.net.au to github.com/garevna/nbn.pineapple.net.au.git:gh-pages

---
+++ precache-manifest.81dfab76ac467c9a4402f1b2a2a1e88d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f684d3ebdfb77c4aaf2d",
+    "url": "/nbn.pineapple.net.au/css/app.91d84ec0.css"
+  },
+  {
+    "revision": "25aeca40c250b477b036",
+    "url": "/nbn.pineapple.net.au/css/chunk-vendors.6a2145aa.css"
+  },
+  {
+    "revision": "32750b00d6f5003097a67377a4af208f",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Bold.32750b00.woff"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "5e111ca086414e1d8905e5a875fb6899",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Bold.5e111ca0.eot"
+  },
+  {
+    "revision": "335c7ebfe69e97fec5ae32fea4fd17fa",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Heavy.335c7ebf.eot"
+  },
+  {
+    "revision": "8d36efeb3349af073647c0c37ec995f1",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Heavy.8d36efeb.ttf"
+  },
+  {
+    "revision": "ffa5800ec1a44bef80af3dfdced37e7a",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Heavy.ffa5800e.woff"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Medium.6444f14a.ttf"
+  },
+  {
+    "revision": "7d5dc1791cf2738d55a87e871549004c",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Medium.7d5dc179.woff"
+  },
+  {
+    "revision": "ce12e7f23ff25c6f534364a7435885df",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Medium.ce12e7f2.eot"
+  },
+  {
+    "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Regular.59525ce1.eot"
+  },
+  {
+    "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Regular.8e8705d0.woff"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/nbn.pineapple.net.au/fonts/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "b439ead5036c39ba1768556b5a272cf8",
+    "url": "/nbn.pineapple.net.au/img/NBN_logo-dark-blue.b439ead5.svg"
+  },
+  {
+    "revision": "c70254e55d28af26c5f200932d060050",
+    "url": "/nbn.pineapple.net.au/img/logo.c70254e5.svg"
+  },
+  {
+    "revision": "84d00cff02475cd70e55a90a316dcdbf",
+    "url": "/nbn.pineapple.net.au/index.html"
+  },
+  {
+    "revision": "f684d3ebdfb77c4aaf2d",
+    "url": "/nbn.pineapple.net.au/js/app.60799cff.js"
+  },
+  {
+    "revision": "25aeca40c250b477b036",
+    "url": "/nbn.pineapple.net.au/js/chunk-vendors.7b0b2ebb.js"
+  },
+  {
+    "revision": "2d0bd16d2987027c93119bdf6ea972c9",
+    "url": "/nbn.pineapple.net.au/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nbn.pineapple.net.au/robots.txt"
+  }
+]);
